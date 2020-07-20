e3eeb6fb89e05c974c30c1233cb3e8c9aa936b53
zacksmash <zachary.warren@me.com>
2020-07-20 09:34:57 -0600

    Remove Vuex/store file

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,7 @@
 import './bootstrap';
-import store from './store';
 import Vue from 'vue';
 import Dashboard from './components/Dashboard.vue';
 
 let app = new Vue({
-  store,
   render: h => h(Dashboard)
 }).$mount('#app')
